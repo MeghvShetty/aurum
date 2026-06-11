c319c542ee5398a545981a05a5044ef8310e4e2c
Megh Shetty <meghvshetty@outlook.com>
2026-06-11 22:57:57 +0100

    added  before_model_callback to agent

--- agents/aurum-agent.py
+++ agents/aurum-agent.py
@@ -2,6 +2,7 @@ from google.adk.agents import LlmAgent
 from google.adk.tools import google_search
 from google.adk.tools.agent_tool import AgentTool
 from utils.instruction_loader import load_instruction_from_file 
+from utils.airs_callbacks import before_model_callback
 
 
 MODEL = "gemini-2.5-pro"
@@ -11,8 +12,8 @@ MODEL = "gemini-2.5-pro"
 portfolio_agent = LlmAgent(
     name = "Portfolio Agent",
     model= MODEL,
-    instruction= load_instruction_from_file("portfolio-instruction.txt")
-
+    instruction= load_instruction_from_file("portfolio-instruction.txt"),
+    before_model_callback=before_model_callback
 )
 
 # Sub Agent 2 : Advisory Agent
@@ -20,6 +21,7 @@ advisory_agent = LlmAgent(
     name = "Advisory Agent",
     model = MODEL,
     instruction= load_instruction_from_file("advisory-instruction.txt")
+    before_model_callback=before_model_callback
 ) 
 
 # Lead Agent 
@@ -28,6 +30,7 @@ lead_agent = LlmAgent(
     model= MODEL,
     instruction= load_instruction_from_file("lead-instruction.txt"),
     tools = [AgentTool(portfolio_agent),AgentTool(advisory_agent), google_search]
+    before_model_callback=before_model_callback
 
 )
 
